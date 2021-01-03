956075f0c7dafa2bfade087acf01e6b6291cfb00
To Dat <todat999@gmail.com>
2021-01-03 13:51:43 +0700

    ..

--- routes/public.js
+++ routes/public.js
@@ -1,6 +1,8 @@
 const route = require('express').Router();
+const RENT_COLL = require('../database/rent_col');
 //MODELS
 const  CAR_MODEL  = require('../models/user_mod');
+const  RENT_MODEL  = require('../models/rent');
 
 //MIDDLEWARE
 const { renderToView }  = require('../utils/childRouting');
@@ -22,9 +24,10 @@ route.get('/register', async (req, res) => {
     renderToView(req, res, 'website/pages/register', {})
 })
 
-route.get('/booking', async (req, res) => {
-    console.log("dsadsadsadsa");
-    renderToView(req, res, 'website/pages/booking', {});
+route.get('/booking/:rentID', async (req, res) => {
+    let { rentID } = req.params;
+    let infoRent = await RENT_MODEL.getInfo({ rentID });
+    renderToView(req, res, 'website/pages/booking', { infoRent: infoRent.data });
 })
 
 module.exports = route;
