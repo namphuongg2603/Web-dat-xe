62fb2ae472bc310328a748f7485c884ba14e88e2
manhttruong <phammanhtruong1999@gmail.com>
2021-01-06 04:12:18 +0700

    .

--- routes/public.js
+++ routes/public.js
@@ -1,11 +1,14 @@
 const route = require('express').Router();
 const RENT_COLL = require('../database/rent_col');
 //MODELS
-const  CAR_MODEL  = require('../models/user_mod');
+const  CAR_MODEL  = require('../models/car');
 const  RENT_MODEL  = require('../models/rent');
+const  CATEGORY_MODEL  = require('../models/category');
 
 //MIDDLEWARE
 const { renderToView }  = require('../utils/childRouting');
+const CAR_COLL = require('../database/car_col');
+const CATEGORY_COLL = require('../database/category_col');
 
 route.get('/home', async (req, res) => {
     renderToView(req, res, 'website/pages/home', {});
@@ -34,6 +37,18 @@ route.get('/booking/:rentID', async (req, res) => {
 })
 route.get('/contact', async (req, res) => {
     renderToView(req, res, 'website/pages/contact', {})
+})
+route.get('/remove/:carID', async (req, res) => {
+    let { carID } = req.params;
+    let infoAfterRemove = await CAR_MODEL.remove({ carID });
+    console.log(infoAfterRemove);
+    res.redirect('/car/list-car')
+})
+route.get('/remove/:categoryID', async (req, res) => {
+    let { categoryID } = req.params;
+    let infoAfterRemove = await CATEGORY_MODEL.remove({ categoryID });
+    console.log(infoAfterRemove);
+    res.redirect('/category/list-category')
 })
 
 
